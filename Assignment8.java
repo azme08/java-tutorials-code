56bff0d2b581220d7bf9efa85512dfe3b6ae5772
azme08 <115527810+azme08@users.noreply.github.com>
2022-12-27 21:08:26 +0600

    Update Assignment8.java

--- Assignment8.java
+++ Assignment8.java
@@ -5,7 +5,20 @@
 // Step 3: if the candidate has passed masters and also have fluent english skill then print you are eligible to for the job interview
 // Step 4: else print Sorry. you are not eligible to for the job interview
 public class Assignment8 {
+  import java.util.Scanner;
   public static void main(String[] args) {
+    char ans1,ans2;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Have you completed your masters?\nChoose your option in small letter:\n y for Yes \n n for No");
+    ans1=sc.next().charAt(0);
     
+    System.out.print("Are you fulent in English?\nCoose your option:\n y for Yes \n n for No");
+     ans2=sc.next().charAt(0);
+    if(ans1 =='y' && ans2=='y'){
+      System.out.println("You are eligible to for the job interview");
+    }
+    else{
+      System.out.println("You are not eligible to for the job interview.");
+    }
   }
 }
