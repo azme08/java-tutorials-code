3df40b405082ce8267004a471bfe8f0ae09ac213
azme08 <115527810+azme08@users.noreply.github.com>
2022-12-27 20:51:42 +0600

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -4,7 +4,23 @@
 // step 3: if user input y / Y then print you are a java lover
 // step 4: if user input n / N then print you are not a java lover
 public class Assignment7 {
+  import java.util.Scanner;
   public static void main(String[] args) {
+    char ans;
+    Scanner sc = new Scanner(System.in);
     
+   System.out.print("Do you love java?\nChoose your ans:Y/y for Yes \nN/n for No \n Ans is:");
+    ans = sc.next().charAt(0);
+    
+    if(ans =='Y' || ans ='y'){
+      System.out.println("You are a java lover.");
+    }
+    else if(ans ='N' || ans ='n'){
+      System.out.println("You are not a java lover.");
+    }
+    else{
+      System.out.println("Your choosing ans is wrong.");
+    }
+                    
   }
 }
